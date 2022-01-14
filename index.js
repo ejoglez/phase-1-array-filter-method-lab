80cacdbc1e3218882dcf2a1ed497d4cbc4bb6ca2
Eric Gonzalez <ejoglez@gmail.com>
2022-01-13 18:14:44 -0600

    commit tree 378e01696026dbac2fc70f76c063b92d024437a0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, string){
+    const results = array.filter(element => string.toLowerCase() === element.toLowerCase());
+    return results;
+}
+
+function fuzzyMatch(array, string){
+    const fuzzyResults = array.filter(element => element.toLowerCase().indexOf(string.toLowerCase()) === 0 );
+    return fuzzyResults;
+}
+
+function matchName(array, string){
+    const nameResults = array.filter(element => element.name.toUpperCase() === string.toUpperCase());
+    return nameResults;
+}
